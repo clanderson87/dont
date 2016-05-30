e4e583c83280368d88239e9e39cc7220fe893201
Chris Anderson <cl.anderson8@gmail.com>
2016-05-30 15:45:00 -0500

    Added comments to buttonKiller. Need to work on flexible implementation.

--- buttonKiller.js
+++ buttonKiller.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
   
-  var idArray = ['submit.add-to-cart', 'submit.one-click-order.x', 'binBtn_btn', 'isCartBtn_btn', 'shGetRates', 'btn-transaction']
+  var amzNames = ['submit.add-to-cart', 'submit.one-click-order.x', 'submit.rcx-subscribe'];//add any Amazon purchase-like button element names to this array
+  var ebayIds = ['binBtn_btn', 'isCartBtn_btn', 'shGetRates']; //add any eBay purchase-like button ID names to this array.
+  var etsyClasses = ['btn-transaction'] //add any Etsy purchase-like button class names to this array.
+  var idArray = amzNames.concat(ebayIds, etsyClasses); 
+  //if you add anynew site arrays, make sure to add them in the above concat statement. 
+  //Also, name them correctly and use the correct document.methodology!
   
   for (var i = 0; i < idArray.length; i++) {
-    if (i < 2) {
+    if (i < 3) {
       try {
         //for Amazon pages
         document.getElementsByName(idArray[i]).style.disabled = true;
