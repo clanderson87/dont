c93de3222edc6e905001de7842ce854bf6e3bd66
Chris Anderson <cl.anderson8@gmail.com>
2016-05-30 15:28:11 -0500

    DRY'd button killer, hopefully. Added icon.png

--- buttonKiller.js
+++ buttonKiller.js
@@ -1,12 +1,31 @@
 document.addEventListener('DOMContentLoaded', function() {
-  //Amazon code 
-  document.getElementsByName('submit.add-to-cart').style.disabled = true;
-  document.getElementsByName('submit.one-click-order.x').style.disabled = true;
-  //eBay code
-  document.getElementById('binBtn_btn').style.disabled = true;
-  document.getElementById('isCartBtn_btn').style.disabled = true;
-  document.getElementById('shGetRates'.style.disabled = true);
-  //Etsy code
-  document.getElementsByClassName('btn-trasaction')
   
-  }, false);
+  var idArray = ['submit.add-to-cart', 'submit.one-click-order.x', 'binBtn_btn', 'isCartBtn_btn', 'shGetRates', 'btn-transaction']
+  
+  for (var i = 0; i < idArray.length; i++) {
+    if (i < 2) {
+      try {
+        //for Amazon pages
+        document.getElementsByName(idArray[i]).style.disabled = true;
+      } catch (error) {
+        continue;
+      }
+    }
+    else if (i > 1 && i < 6) {
+      try {
+        //for eBay pages
+        document.getElementById(idArray[i]).style.disabled = true;
+      } catch (error) {
+        continue;
+      }
+    }
+    else {
+      try {
+        //for Etsy pages
+        documents.getElementsByClassName(idArray[i]).style.disabled = true;
+      } catch (error) {
+        continue;
+      }
+    }
+  }
+  }, false)
