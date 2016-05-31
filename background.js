ee10f5266497bc6cceca2b138cb41d19450f7394
Chris Anderson <cl.anderson8@gmail.com>
2016-05-30 20:04:39 -0500

    Finally disabling etsy buttons, but styling needs to happen.

---
+++ background.js
@@ -0,0 +1,62 @@
+var go = function() {
+  
+  console.log("background.js is live!!")
+  
+  var amzIds = ['add-to-cart-button', 'oneClickBuyButton', 'submit.rcx-subscribe'];//add any Amazon purchase-like button element names to this array
+  var ebayIds = ['binBtn_btn', 'isCartBtn_btn', 'shGetRates']; //add any eBay purchase-like button ID names to this array.
+  var etsyClasses = ['btn-transaction'] //add any Etsy purchase-like button class names to this array.
+  var url = window.location.href;
+  
+  console.log("url is: ", url)
+  
+  if (url.indexOf('amazon.com') > -1) {
+    for (var i = 0; i < amzIds.length; i++) {
+      if (i = 3) {
+        try {
+          document.getElementsByName(amzIds[i]).style.disabled = true;
+        } catch (error) {
+          console.log(e);
+          continue;
+        }
+      }
+      try{
+        document.getElementById(amzIds[i]).style.disabled = true;
+      }catch (e){
+        console.log(e);
+        continue;
+      }
+    }
+  }
+  
+  if(url.indexOf('ebay.com') > -1) {
+    for (var i = 0; i < ebayIds.length; i++) {
+      try{
+        document.getElementById(ebayIds[i]).disabled = true;
+      }catch (e){
+        console.log(e);
+        continue;
+      }
+    }
+  }
+  
+  if(url.indexOf('etsy.com') > -1) {
+    for (var i = 0; i < etsyClasses.length; i++) {
+      try{
+        console.log("etsyClasses is: ", typeof(etsyClasses[i]), etsyClasses[i]);
+        var thing = document.getElementsByClassName(etsyClasses[i]);
+        console.log(thing);
+        thing[0].disabled = true;
+        thing[0].style.disabled = true;
+      }catch (e){
+        console.log(e);
+        continue;
+      }
+    }
+  }
+  
+  console.log("Just don't.")
+
+  };
+  
+   setTimeout(go, 3500);
+  
